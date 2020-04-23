80e05269d024378d7a4783a76a5d3a4b7f919353
erkanturker <erkanturker17@gmail.com>
2020-04-23 16:14:49 -0400

    Day 3 Selenium just adding static method

--- src/test/java/tests/day03/navigationPractice.java
+++ src/test/java/tests/day03/navigationPractice.java
@@ -9,6 +9,12 @@ public class navigationPractice {
         WebDriver driver= BrowserFactory.getDriver ( "chrome" );
         driver.manage ().window ().maximize ();
         driver.get ( "http://google.com" );
+        BrowserUtils.wait ( 3 );
+        // wait for 3 second
+        // this is out custom method
+        // since method is static, we can use lass name to call the method
+        // as a parameter we provide number of seconds
+
         driver.navigate ().to ( "http://amazon.com" );
         //navigate back to google.com
         driver.navigate ().back ();
